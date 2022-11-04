066ec99362fa1fc385cb0e15352b935e85b46923
ErikH <75594114+ErikKN1@users.noreply.github.com>
2022-11-03 20:20:40 -0600

    Caracter a codigo ACSII v 1.1

--- src/acsiicode/AcsiiCode.java
+++ src/acsiicode/AcsiiCode.java
@@ -19,12 +19,28 @@ public class AcsiiCode {
      */
     public static void main(String[] args) {
         Scanner InDate=new Scanner(System.in);
-        char code;
-        System.out.println("Enter character to transform");
-        code=InDate.next().charAt(0); //pasamos caracter a codigo
-        int codeA=(int)code;
-        System.out.println("El caracter " + code + " convertido a ACSII ES " +codeA );
-        
+        int Option, code;
+        char character;
+        System.out.println("what do you want to do?\n 1-convert from code to character \n 2-convert character to code");
+        Option=InDate.nextInt();
+        switch(Option){
+            case 1:
+                   System.out.println("convert from code to character ");
+                   System.out.println("Enter code to transform ");
+                   code=InDate.nextInt();
+                   character=(char)code;
+                   System.out.println("the code " + code + " transformed to ACSII code is: "+character);
+            break;
+            case 2:
+                System.out.println("convert from character to code ");
+                System.out.println("Enter character to transform");
+                character=InDate.next().charAt(0); //pasamos caracter a codigo
+                code=(int)character;
+                System.out.println("El caracter " + character + " convertido a ACSII ES " +code );
+            break;
+            default:
+                System.out.println("Invalid Option");
+            break;                
+        }    
     }
-    
 }
