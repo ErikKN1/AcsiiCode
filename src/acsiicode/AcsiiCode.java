17c70a7dc2c6486522421b2c8e0b3cd72630be5c
ErikH <75594114+ErikKN1@users.noreply.github.com>
2022-11-03 19:07:08 -0600

    Caracter a codigo ACSII v 1.0

---
+++ src/acsiicode/AcsiiCode.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package acsiicode;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author erik.hernandez
+ */
+public class AcsiiCode {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner InDate=new Scanner(System.in);
+        char code;
+        System.out.println("Enter character to transform");
+        code=InDate.next().charAt(0); //pasamos caracter a codigo
+        int codeA=(int)code;
+        System.out.println("El caracter " + code + " convertido a ACSII ES " +codeA );
+        
+    }
+    
+}
